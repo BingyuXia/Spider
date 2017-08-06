6b4c154234922ef57c194424c669929d9418f2e8
Bingyu Xia <xiaby14@mails.tsinghua.edu.cn>
2017-08-06 20:50:07 +0800

    output-apdate

--- guba/guba/afterprocessing.py
+++ guba/guba/afterprocessing.py
@@ -1,24 +1,62 @@
 import pickle
 import pymongo
+from stocks_list import STOCKS_LIST
+
+host = "127.0.0.1"
+port = "27017"
+
+client = pymongo.MongoClient(host, port)
+db_name_1 = "YQ_BASE"
+db_name_2 = "YQ_OUTPUT"
+col_raw = client[db_name_1]["GUBA_RAW"]
+col_d = client[db_name_1]["GUBA_D_20170806"]
+col_output = client[db_name_2]["OUTPUT_20170806"]
+
+
+for stock in STOCKS_LIST:
+	show_d = col_d.find({"ID":stock}, {"_id" : 0}) 
+	items = list(show_d)
+	post_add = 0
+	content_charactor_count = 0
+	comments_charactor_count = 0
+	faces_count = 0
+	for item in items:
+		show_raw = col_raw.find({"ID":item["ID"], "TITLE":item["TITLE"], "TIME":item["TIME"], 
+	        "WRITER":item["WRITER"]}, {"_id":0, "READ":1, "COMMENT":1})
+
+		if show.count() == 0:
+			post_add += 1
+		    read_old = 0
+		    comment_old =0
+		    doc = {"ID":item["ID"], "TITLE":item["TITLE"], "TIME":item["TIME"], "WRITER":item["WRITER"],
+		    		"READ":item["READ"], "COMMENT":item["COMMENT"],}
+	    	col_raw.insert_one(doc)
+		else:
+		    post = list(show)[0]
+		    read_old = post["READ"]
+		    comment_old = post["COMMENT"]
+		    show_raw.update_one({'$set' : {"READ" : item["READ"], "COMMENT": item["COMMENT"]}})
+
+		for i in item["CONTENT"]:
+			content_charactor_count += len(i)
+		for i in item["COMMENTS"]:
+			for j in i:
+				comments_charactor_count += len(j)
+		for i in item["FACE"]:
+			faces_count += len(i)
+
+		read_add = item["READ"] - read_old
+		comment_add = item["COMMENT"] - comment_old
+
+		doc = {"ID":item["ID"], "POST_ADD": post_add, "READ_ADD":read_add, "COMMENT_ADD":comment_add, 
+			       "CONTENT_C":content_charactor_count, "COMMENTS_C":comments_charactor_count, 
+			       "FACE_C":faces_count}
+
+		col_output.insert_one(doc)
 
 
 
-show = self.col.find({"ID":item["ID"], "TITLE":item["TITLE"], "TIME":item["TIME"], \
-    "WRITER":item["WRITER"]}, {"_id":0, "READ":1, "COMMENT":1})
-if show.count() == 0:
-    read_old = 0
-    comment_old =0
-    doc = {key:item[key] for key in item}
-    self.col.insert_one(doc)
-else:
-    post = list(show)[0]
-    read_old = post["READ"]
-    comment_old = post["COMMENT"]
-    show.update_one({'$set' : {"READ" : item["READ"], "COMMENT": item["COMMENT"]}})
-read_add = item["READ"] - read_old
-comment_add = item["COMMENT"] - comment_old
-doc = {"ID":item["ID"], "READ":read_add, "COMMENT":comment_add}
-self.col2.insert_one(doc)
+
 
 #评论：平均字数， 空评论数， 长评论数
 #表情： 表情个数及占比，正负面表情个数及占比 
