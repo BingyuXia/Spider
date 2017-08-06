c8047a74b504ec378edff5c94f966a7e0a5aee9d
Bingyu Xia <xiaby14@mails.tsinghua.edu.cn>
2017-08-06 13:04:37 +0800

    spider-apdate

--- guba/guba/settings.py
+++ guba/guba/settings.py
@@ -70,8 +70,8 @@ ITEM_PIPELINES = {
 #MongDB
 MONGODB_HOST = "127.0.0.1"
 MONGODB_PORT = 27017
-MONGODB_DBNAME = "GUBA"
-MONGODB_COLNAME = "TEMP"
+MONGODB_DBNAME = "YQ_BASE"
+MONGODB_COLNAME = "GUBA_TEMP"
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
